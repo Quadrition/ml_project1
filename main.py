edcda836167c9630ee3853c3e14a53f9590b2aca
Petar Cerovic <quadrition@gmail.com>
2020-07-02 00:16:17 +0200

    Added normalize_data method

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import pandas as pd
+import numpy as np
+
+
+def normalize_data(data):
+    cols = len(data[0])
+
+    for col in range(cols):
+        column_data = []
+        for row in data:
+            column_data.append(row[col])
+
+        mean = np.mean(column_data)
+        std = np.std(column_data)
+
+        for row in data:
+            row[col] = (row[col] - mean) / std
+
+
 def main():
-    pass
+    df = pd.read_csv('credit_card_data.csv')
+    df = df.fillna(df.median())
+    data = df.iloc[:, 1:].values
+
+    normalize_data(data)
+    # print(data)
 
 
 if __name__ == '__main__':
