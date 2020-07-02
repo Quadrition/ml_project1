5a2805c877b4fd8fbf6deb5ee7bbfbc5a634a21a
Petar Cerovic <quadrition@gmail.com>
2020-07-02 02:42:25 +0200

    Added analize data with pca

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from kmeans import KMeans
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 import numpy as np
 
 
@@ -17,13 +20,74 @@ def normalize_data(data):
             row[col] = (row[col] - mean) / std
 
 
-def main():
+def analyze_with_pca():
     df = pd.read_csv('credit_card_data.csv')
     df = df.fillna(df.median())
     data = df.iloc[:, 1:].values
 
     normalize_data(data)
-    # print(data)
+
+    pca = PCA()
+    pca.fit(data)
+
+    plt.plot(range(1, 18), pca.explained_variance_ratio_.cumsum(), marker='o', linestyle='--')
+    plt.xlabel('Features')
+    plt.ylabel('Variance')
+    plt.show()
+
+    features = 7
+    pca = PCA(n_components=features)
+    pca.fit(data)
+    scores = pca.transform(data)
+
+    plt.bar(range(pca.n_components_), pca.explained_variance_ratio_, color='black')
+    plt.xlabel('PCA features')
+    plt.ylabel('Variance %')
+    plt.xticks(range(pca.n_components_))
+    plt.show()
+
+    # plot_optimal_k(data)
+
+    n_clusters = 9
+
+    k_means = KMeans(n_clusters=n_clusters, max_iter=100)
+    k_means.fit(scores)
+
+    colors = {0: 'red', 1: 'green', 2: 'blue', 3: 'purple', 4: 'orange', 5: 'cyan', 6: 'yellow', 7: 'indigo', 8: 'pink',
+              9: 'black'}
+    plt.figure()
+    for idx, cluster in enumerate(k_means.clusters):
+        plt.scatter(cluster.center[0], cluster.center[1], c=colors[idx], marker='x', s=100)
+        for datum in cluster.data:
+            plt.scatter(datum[0], datum[1], c=colors[idx])
+
+    plt.xlabel('Component 1')
+    plt.ylabel('Component 2')
+    plt.show()
+
+
+def analyze_without_pca():
+    pass
+
+
+def plot_optimal_k(data):
+    plt.figure()
+    sum_squared_errors = []
+    for n_clusters in range(1, 20):
+        k_means = KMeans(n_clusters=n_clusters, max_iter=100)
+        k_means.fit(data)
+        sse = k_means.sum_squared_error()
+        sum_squared_errors.append(sse)
+    print(sum_squared_errors)
+    plt.plot(range(1, 20), sum_squared_errors)
+    plt.xlabel('# of clusters')
+    plt.ylabel('WCSSE')
+    plt.show()
+
+
+def main():
+    analyze_with_pca()
+    # analyze_without_pca()
 
 
 if __name__ == '__main__':
